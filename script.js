dd0b3cedf953a0b11fb57ae8b6dd3fa914f47cab
w1f3s <amandabispo800@gmail.com>
2024-09-24 19:31:28 -0300

    Adicionando assinatura script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const avanca = document.querySelectorAll('.btn-proximo');
-
+//Amanda Bispo 3D 24/09/2024
 avanca.forEach(button => {
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
